32b2aacc2041113b02431c89f37847dc3c7f1d7f
Jonathan Reinink <jonathan@reininks.com>
2013-07-11 14:14:15 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,180 @@
+Roundabout
+==========
+
+RESTful router based on the Symfony2 HttpFoundation.
+
+## Highlights
+
+- Extremely simple API
+- RESTful support: GET, POST, PUT & DELETE
+- Based on the Symfony2 HttpFoundation (it just works)
+- Supports closures, functions and controller (class) callbacks
+- Ability to bulk imports routes using arrays
+- Custom controller instantiation (for IoC container integration)
+
+## Setup
+
+```php
+<?php
+
+use \Reinink\Roundabout\Router;
+use \Symfony\Component\HttpFoundation\Request;
+
+// Create request object
+$request = Request::createFromGlobals();
+
+// Create router
+$router = new Router($request);
+
+```
+
+## Basic routes
+
+```php
+<?php
+
+// Home page
+$router->get(
+    '/',
+    function () {
+        // do something
+    }
+);
+
+// Contact page
+$router->get(
+    '/contact',
+    function () {
+        // do something
+    }
+);
+
+// Process form post
+$router->post(
+    '/form-submit',
+    function () {
+        // do something
+    }
+);
+
+// Secure (HTTPS) page
+$router->getSecure(
+    '/login',
+    function () {
+        // do something
+    }
+);
+
+// PUT request
+$router->put(
+    '/resource',
+    function () {
+        // do something
+    }
+);
+
+// DELETE request
+$router->delete(
+    '/resource',
+    function () {
+        // do something
+    }
+);
+```
+
+## More complicated routes
+
+```php
+<?php
+
+// User profile
+$router->get(
+    '/user/([0-9]+)',
+    function ($userId) {
+        // do something with $userId
+    }
+);
+
+// Output image
+$router->get(
+    '/photo/(xlarge|large|medium|small|xsmall)/([0-9]+)',
+    function ($imageSize, $imageId) {
+        // do something with $imageSize and $imageId
+    }
+);
+```
+
+## Working with controller classes
+
+```php
+<?php
+
+// Home page
+$router->get('/', 'Controller::index');
+
+// Contact page
+$router->get('/contact', 'Controller::contact');
+
+// Process form post
+$router->post('/form-submit', 'Controller::process_form');
+
+// Secure (HTTPS) page
+$router->getSecure('/login', 'Controller::login');
+```
+
+## Bulk route definitions
+
+```php
+<?php
+
+$router->import(
+    [
+        [
+            'path' => '/',
+            'method' => 'GET',
+            'secure' => false,
+            'callback' => 'Controller::index'
+        ],
+        [
+            'path' => '/contact',
+            'method' => 'GET',
+            'secure' => false,
+            'callback' => 'Controller::contact'
+        ],
+        [
+            'path' => '/form-submit',
+            'method' => 'POST',
+            'secure' => false,
+            'callback' => 'Controller::process_form'
+        ],
+        [
+            'path' => '/login',
+            'method' => 'GET',
+            'secure' => true,
+            'callback' => 'Controller::login'
+        ]
+    ]
+);
+```
+
+## Custom controller instantiation
+
+By default Roundabout instantiates controller classes automatically, but this beahavior can be overridden using the `$instantiation_callback` paramater. This can be helpful in situations where you want to use an inversion of control container.
+
+```php
+<?php
+
+// Your IoC container
+$ioc = new Container;
+
+// Create router
+$router = new Router(
+    $request,
+    function ($class, $method, $parameters) use ($ioc) {
+
+        $controller = $ioc->make($class);
+
+        return call_user_func_array(array($controller, $method), $parameters);
+    }
+);
+```
---
+++ src/Reinink/Roundabout/Router.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Reinink\Roundabout;
+
+class Router
+{
+    protected $request;
+    protected $instantiation_callback;
+    protected $routes;
+
+    public function __construct(\Symfony\Component\HttpFoundation\Request $request, Callable $instantiation_callback = null)
+    {
+        $this->request = $request;
+        $this->instantiation_callback = $instantiation_callback;
+        $this->routes = [];
+    }
+
+    public function get($path, $callback)
+    {
+        $this->bind($path, 'get', false, $callback);
+    }
+
+    public function getSecure($path, $callback)
+    {
+        $this->bind($path, 'get', true, $callback);
+    }
+
+    public function post($path, $callback)
+    {
+        $this->bind($path, 'post', false, $callback);
+    }
+
+    public function postSecure($path, $callback)
+    {
+        $this->bind($path, 'post', true, $callback);
+    }
+
+    public function put($path, $callback)
+    {
+        $this->bind($path, 'put', false, $callback);
+    }
+
+    public function putSecure($path, $callback)
+    {
+        $this->bind($path, 'put', true, $callback);
+    }
+
+    public function delete($path, $callback)
+    {
+        $this->bind($path, 'delete', false, $callback);
+    }
+
+    public function deleteSecure($path, $callback)
+    {
+        $this->bind($path, 'delete', true, $callback);
+    }
+
+    public function import(Array $routes)
+    {
+        foreach ($routes as $route) {
+
+            if (!isset($route['path'])) {
+                trigger_error('The "path" variable is missing from imported route.', E_USER_ERROR);
+            }
+
+            if (!isset($route['callback'])) {
+                trigger_error('The "callback" variable is missing from imported route.', E_USER_ERROR);
+            }
+
+            if (!isset($route['secure'])) {
+                $route['secure'] = false;
+            }
+
+            if (!isset($route['method'])) {
+                $route['method'] = 'GET';
+            }
+
+            $this->bind($route['path'], $route['method'], $route['secure'], $route['callback']);
+        }
+    }
+
+    public function bind($path, $method, $secure, $callback)
+    {
+        if (!is_string($path)) {
+            trigger_error('Route path must be a string, ' . gettype($path) . ' given.', E_USER_ERROR);
+        }
+
+        if (!is_string($method)) {
+            trigger_error('Route method must be a string, ' . gettype($method) . ' given.', E_USER_ERROR);
+        }
+
+        if (!is_bool($secure)) {
+            trigger_error('Route secure must be a boolean, ' . gettype($secure) . ' given.', E_USER_ERROR);
+        }
+
+        if (!is_callable($callback, true)) {
+            trigger_error('Route callback must be a valid callback, ' . gettype($callback) . ' given.', E_USER_ERROR);
+        }
+
+        $this->routes[] =
+        [
+            'path' => $path,
+            'method' => 'get',
+            'secure' => false,
+            'callback' => $callback
+        ];
+    }
+
+    private function match(&$route)
+    {
+        if (!$this->request->isMethod($route['method'])) {
+            return false;
+        }
+
+        if ($this->request->isSecure() !== $route['secure']) {
+            return false;
+        }
+
+        if (preg_match('#^' . $route['path'] . '$#', $this->request->getPathInfo(), $matches) === 1) {
+
+            array_shift($matches);
+
+            $route['parameters'] = $matches;
+
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function run()
+    {
+        foreach ($this->routes as $route) {
+
+            if ($this->match($route)) {
+
+                if (is_object($route['callback']) or function_exists($route['callback'])) {
+
+                    return call_user_func_array($route['callback'], $route['parameters']);
+
+                } else if (is_string($route['callback'])) {
+
+                    list($class, $method) = explode('::', $route['callback']);
+
+                    if (is_null($this->instantiation_callback)) {
+                        return call_user_func_array(array(new $class, $method), $route['parameters']);
+                    } else {
+                        return call_user_func_array($this->instantiation_callback, array($class, $method, $route['parameters']));
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
